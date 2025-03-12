7dbdecbf8dc1360d50ca15b4ce892fa2ec504bee
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-03-12 17:30:07 +0900

    Update 사칙연산.java

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,19 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-
-		Scanner scanner = new Scanner(System.in);
-		
-		int A = scanner.nextInt();
-		int B = scanner.nextInt();
-	
-		System.out.println(A+B);
-		System.out.println(A-B);
-		System.out.println(A*B);
-		System.out.println(A/B);
-		System.out.println(A%B);
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println((A+B)+"\n"+(A-B)+"\n"+(A*B)+"\n"+(A/B)+"\n"+(A%B));
+    }
 }
