a7d005fe13a7e8a2ef31a77a79b61e78e9034c9b
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-03-12 19:05:11 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 112 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,13 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) {
-
-		Scanner scanner = new Scanner(System.in);
-		
-		int y = scanner.nextInt();
-		System.out.println(y-543);
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        System.out.println(a-543);
+    }
 }
