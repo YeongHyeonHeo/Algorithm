2ef153f705829eb634ed8c492c4e7e2af58f3f60
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-03-25 22:35:43 +0900

    [Silver II] Title: 나무 자르기, Time: 512 ms, Memory: 119448 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.rmi.MarshalException;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[] A;
+
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        A = new int[N + 1];
+        for (int i = 1; i <= N; i++) A[i] = scan.nextInt();
+    }
+
+    // 결정 문제(뒤집은 문제)를 풀어줄 함수
+    static boolean determination(int H) {
+        // H 높이로 나무들을 잘랐을 때, M 만큼을 얻을 수 있으면 t, 없으면 f를 return하는 함수
+        long sum = 0; // long 타입 주의!!
+        for (int i = 1; i <= N; i++) {
+            if (A[i] > H) sum += A[i] - H;
+        }
+        return sum >= M;
+    }
+
+    static void pro() {
+        int L = 0, R = 1000000000, ans = 0;
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (determination((int)mid)) {
+                ans = mid;
+                L = mid + 1;
+            }
+            else R = mid - 1;
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
