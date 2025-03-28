78a94c3b783afca90553e122a26a72cb761c36f8
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-03-28 17:15:42 +0900

    [Gold IV] Title: 공유기 설치, Time: 360 ms, Memory: 41792 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -16,16 +16,11 @@ public class Main {
     }
 
     static boolean determination(int D) {
-        // D 만큼의 거리 차이를 둔다면 C 개 만큼의 공유기를 설치할 수 있는가?
-
-        // 제일 왼쪽 집부터 가능한 많이 설치해보자
-        // D 만큼의 거리를 두면서 최대로 설치한 개수랑 C 를 비교하자
         int cnt = 1, last = A[1];
         for (int i = 2; i <= N; i++) {
-            // 이번에 A[i]에 설치가 가능한가?
             if (A[i] - last >= D) {
-                cnt++;
                 last = A[i];
+                cnt++;
             }
         }
         return cnt >= C;
@@ -33,7 +28,6 @@ public class Main {
 
     static void pro() {
         Arrays.sort(A, 1, N + 1);
-
         int L = 1, R = 1000000000, ans = 0;
         while (L <= R) {
             int mid = (L + R) / 2;
