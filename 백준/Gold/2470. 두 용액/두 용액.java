182b27fe4316faecaa902b95c693ed6c669184b9
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-03-29 01:06:43 +0900

    [Gold V] Title: 두 용액, Time: 336 ms, Memory: 28168 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -14,38 +14,22 @@ public class Main {
         for (int i = 1; i <= N; i++) A[i] = scan.nextInt();
     }
 
-    static int lower_bound(int[] A, int L, int R, int X) {
-        int res = R + 1;
-        while (L <= R) {
-            int mid = (L + R) / 2;
-            if (A[mid] >= X) {
-                res = mid;
-                R = mid - 1;
-            } else L = mid + 1;
-        }
-        return res;
-    }
-
     static void pro() {
         Arrays.sort(A, 1, N + 1);
 
         int best_sum = Integer.MAX_VALUE;
-        int v1 = 0, v2 = 0;
-        for (int left = 1; left <= N - 1; left++) {
-            int res = lower_bound(A, left+1, N, -A[left]);
+        int v1 = 0, v2 = 0, L = 1, R = N;
 
-            // A[res - 1]와 A[res] 중에 A[left]와 섞었을 때의 정보를 갱신
-            if (left + 1 <= res - 1 && Math.abs(A[left] + A[res - 1]) < best_sum) {
-                best_sum = Math.abs(A[left] + A[res - 1]);
-                v1 = A[left];
-                v2 = A[res - 1];
+        while (L < R) { // L == R인 상황이면 용액이 한 개 뿐인 것이므로, L < R일 때까지만 반복
+            int sum = A[L] + A[R];
+            if (Math.abs(sum) < best_sum) {
+                best_sum = Math.abs(sum);
+                v1 = A[L];
+                v2 = A[R];
             }
 
-            if (res <= N && Math.abs(A[left] + A[res]) < best_sum) {
-                best_sum = Math.abs(A[left] + A[res]);
-                v1 = A[left];
-                v2 = A[res];
-            }
+            if (sum > 0) R--;
+            else L++;
         }
         sb.append(v1).append(' ').append(v2);
         System.out.println(sb);
