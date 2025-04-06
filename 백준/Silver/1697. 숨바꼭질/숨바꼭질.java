81e3b89fee95856b03b3676cb94adcf8e5c5e2e4
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-04-06 21:36:33 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 18628 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -7,36 +7,46 @@ public class Main {
 
     static int N, K;
     static int[] dist;
-    static int[] dir = {1, -1, 2};
+    static boolean[] visit;
 
     static void input() {
         N = scan.nextInt();
         K = scan.nextInt();
-        dist = new int[100000 + 1];
+        visit = new boolean[100005];
+        dist = new int[100005];
     }
 
-    static void bfs(int start) {
-        for (int i = 0; i <= 100000; i++) dist[i] = -1;
+    static void bfs() {
         Queue<Integer> Q = new LinkedList<>();
-        Q.add(start);
-        dist[start] = 0;
+        Q.add(N);
+        visit[N] = true;
+        dist[N] = 0;
 
         while (!Q.isEmpty()) {
             int x = Q.poll();
-            for (int k = 0; k < 3; k++) {
-                int nx;
-                if (dir[k] == 2) nx = x * dir[k];
-                else nx = x + dir[k];
-                if (nx < 0 || nx > 100000) continue;
-                if (dist[nx] != -1) continue;
-                Q.add(nx);
-                dist[nx] = dist[x] + 1;
+            int y = x - 1;
+            if (0 <= y && y <= 100000 && !visit[y]) {
+                visit[y] = true;
+                dist[y] = dist[x] + 1;
+                Q.add(y);
+            }
+            y = x + 1;
+            if (0 <= y && y <= 100000 && !visit[y]) {
+                visit[y] = true;
+                dist[y] = dist[x] + 1;
+                Q.add(y);
+            }
+            y = x * 2;
+            if (0 <= y && y <= 100000 && !visit[y]) {
+                visit[y] = true;
+                dist[y] = dist[x] + 1;
+                Q.add(y);
             }
         }
     }
 
     static void pro() {
-        bfs(N);
+        bfs();
         System.out.println(dist[K]);
     }
 
