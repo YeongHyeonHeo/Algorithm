af6d6cbac0235307c028bec59e32357bdde97d2a
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-04-07 21:54:02 +0900

    [Gold IV] Title: 탈출, Time: 112 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -5,34 +5,29 @@ public class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int N, M;
-    static String[] a;
+    static int R, C;
+    static String[] A;
     static int[][] dist_water, dist_hedgehog;
-    static boolean[][] visit;
     static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
     static void input() {
-        N = scan.nextInt();
-        M = scan.nextInt();
-        a = new String[N];
-        for (int i = 0; i < N; i++) a[i] = scan.nextLine();
-        visit = new boolean[N][M];
-        dist_water = new int[N][M];
-        dist_hedgehog = new int[N][M];
+        R = scan.nextInt();
+        C = scan.nextInt();
+        A = new String[R];
+        for (int i = 0; i < R; i++) A[i] = scan.nextLine();
+        dist_water = new int[R][C];
+        dist_hedgehog = new int[R][C];
     }
 
     static void bfs_water() {
         Queue<Integer> Q = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                dist_water[i][j] = -1;
-                visit[i][j] = false;
-                if (a[i].charAt(j) == '*') {
-                    Q.add(i);
-                    Q.add(j);
-                    dist_water[i][j] = 0;
-                    visit[i][j] = true;
-                }
+
+        for (int i = 0; i < R; i++) for (int j = 0; j < C; j++) {
+            dist_water[i][j] = -1;
+            if (A[i].charAt(j) == '*') {
+                Q.add(i);
+                Q.add(j);
+                dist_water[i][j] = 0;
             }
         }
 
@@ -40,29 +35,25 @@ public class Main {
             int x = Q.poll(), y = Q.poll();
             for (int k = 0; k < 4; k++) {
                 int nx = x + dir[k][0], ny = y + dir[k][1];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if (a[nx].charAt(ny) != '.') continue;
-                if (visit[nx][ny]) continue;
+                if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
+                if (A[nx].charAt(ny) != '.') continue;
+                if (dist_water[nx][ny] != -1) continue;
                 Q.add(nx);
                 Q.add(ny);
                 dist_water[nx][ny] = dist_water[x][y] + 1;
-                visit[nx][ny] = true;
             }
         }
     }
 
     static void bfs_hedgehog() {
         Queue<Integer> Q = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                dist_hedgehog[i][j] = -1;
-                visit[i][j] = false;
-                if (a[i].charAt(j) == 'S') {
-                    Q.add(i);
-                    Q.add(j);
-                    dist_hedgehog[i][j] = 0;
-                    visit[i][j] = true;
-                }
+
+        for (int i = 0; i < R; i++) for (int j = 0; j < C; j++) {
+            dist_hedgehog[i][j] = -1;
+            if (A[i].charAt(j) == 'S') {
+                Q.add(i);
+                Q.add(j);
+                dist_hedgehog[i][j] = 0;
             }
         }
 
@@ -70,32 +61,24 @@ public class Main {
             int x = Q.poll(), y = Q.poll();
             for (int k = 0; k < 4; k++) {
                 int nx = x + dir[k][0], ny = y + dir[k][1];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if (a[nx].charAt(ny) != '.' && a[nx].charAt(ny) != 'D') continue;
-                if (dist_water[nx][ny] != -1 && dist_water[nx][ny] <= dist_hedgehog[x][y] + 1) continue;
-                if (visit[nx][ny]) continue;
+                if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
+                if (A[nx].charAt(ny) != '.' && A[nx].charAt(ny) != 'D') continue;
+                if (dist_water[nx][ny] != -1 && dist_hedgehog[x][y] + 1 >= dist_water[nx][ny]) continue;
+                if (dist_hedgehog[nx][ny] != -1) continue;
                 Q.add(nx);
                 Q.add(ny);
                 dist_hedgehog[nx][ny] = dist_hedgehog[x][y] + 1;
-                visit[nx][ny] = true;
             }
         }
     }
 
     static void pro() {
-        // 각 칸마다 물에 닿는 시간 계산
         bfs_water();
-
-        // 고슴도치가 물을 피해 탐색할 수 있는 공간 찾기
         bfs_hedgehog();
-
-        // 탈출구 'D'에 대한 결과 출력
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (a[i].charAt(j) == 'D') {
-                    if (dist_hedgehog[i][j] == -1) System.out.println("KAKTUS");
-                    else System.out.println(dist_hedgehog[i][j]);
-                }
+        for (int i = 0; i < R; i++) for (int j = 0; j < C; j++) {
+            if (A[i].charAt(j) == 'D') {
+                if (dist_hedgehog[i][j] == -1) System.out.println("KAKTUS");
+                else System.out.println(dist_hedgehog[i][j]);
             }
         }
     }
