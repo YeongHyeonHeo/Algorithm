621983de746c58707ed8372376e93797eb26b34f
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-04-12 19:01:14 +0900

    [Silver III] Title: 물 주기, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/23351. 물 주기/물 주기.java
+++ 백준/Silver/23351. 물 주기/물 주기.java
@@ -6,41 +6,20 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
 
     static int N, K, A, B, day;
-    static int[] water;
-    static boolean dead;
 
     static void input() {
         N = scan.nextInt();
         K = scan.nextInt();
         A = scan.nextInt();
         B = scan.nextInt();
-        water = new int[N + 1];
-        for (int i = 1; i <= N; i++) water[i] = K;
     }
 
     static void pro() {
-        while (true) {
-            for (int L = 1; L <= N-A+1; L+=A) {
-                day++;
-                int R = L + A - 1;
-                for (int i = L; i <= R; i++) water[i] += B-1;
-                for (int i = 1; i < L; i++) {
-                    water[i]--;
-                    if (water[i] == 0) {
-                        dead = true;
-                        break;
-                    }
-                }
-                for (int i = R+1; i <= N; i++) {
-                    water[i]--;
-                    if (water[i] == 0) {
-                        dead = true;
-                        break;
-                    }
-                }
-                if (dead) break;
-            }
-            if (dead) break;
+        N /= A;
+        while (K > 0) {
+            day++;
+            K--;
+            if (day % N == 0) K += B;
         }
         System.out.println(day);
     }
