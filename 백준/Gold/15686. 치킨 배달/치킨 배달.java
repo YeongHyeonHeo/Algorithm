cdabdf39ec858d9b3e76ccc2eda9a026349d187f
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-04-13 01:15:05 +0900

    [Gold V] Title: 치킨 배달, Time: 156 ms, Memory: 15356 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -5,17 +5,21 @@ public class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int N, M, chicken, home, erase_cnt, ans;
-    static int[][] A, chic_arr, home_arr;
+    static int N, M, home, chicken, ans;
+    static int[][] A, home_arr, chick_arr;
+    static int[] selected;
 
     static void input() {
         N = scan.nextInt();
         M = scan.nextInt();
         A = new int[N + 1][N + 1];
-        chic_arr = new int[14][2];
         home_arr = new int[2*N + 1][2];
+        chick_arr = new int[14][2];
+        selected = new int[M + 1];
         for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) A[i][j] = scan.nextInt();
+            for (int j = 1; j <= N; j++) {
+                A[i][j] = scan.nextInt();
+            }
         }
         ans = Integer.MAX_VALUE;
     }
@@ -23,54 +27,47 @@ public class Main {
     static void chick_dist() {
         int dist_sum = 0;
         for (int i = 1; i <= home; i++) {
-            int min = Integer.MAX_VALUE;
-            for (int j = 1; j <= chicken; j++) {
-                if (A[chic_arr[j][0]][chic_arr[j][1]] == 2) {
-                    int dist = Math.abs(home_arr[i][0] - chic_arr[j][0]) + Math.abs(home_arr[i][1] - chic_arr[j][1]);
-                    min = Math.min(min, dist);
-                }
+            int home_min = Integer.MAX_VALUE;
+            for (int j = 1; j <= M; j++) {
+                int chic = selected[j];
+                int dist = Math.abs(home_arr[i][0] - chick_arr[chic][0]) + Math.abs(home_arr[i][1] - chick_arr[chic][1]);
+                home_min = Math.min(home_min, dist);
             }
-            dist_sum += min;
+            dist_sum += home_min;
         }
         ans = Math.min(ans, dist_sum);
     }
 
-    static void dfs(int idx, int cnt) {
-        if (cnt == erase_cnt) {
-            // 도시의 치킨거리 계산
+    static void rec_func(int k) {
+        if (k == M + 1) {
             chick_dist();
-            return;
+        } else {
+            for (int cand = selected[k-1] + 1; cand <= chicken; cand++) {
+                selected[k] = cand;
+                rec_func(k + 1);
+            }
         }
-        if (idx > chicken) return;
-
-        A[chic_arr[idx][0]][chic_arr[idx][1]] = 0;
-        dfs(idx + 1, cnt + 1);
-
-        A[chic_arr[idx][0]][chic_arr[idx][1]] = 2;
-        dfs(idx + 1, cnt);
     }
 
     static void pro() {
+        // 집, 치킨집 모으기
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                // 치킨집 위치 모으기
-                if (A[i][j] == 2) {
-                    chicken++;
-                    chic_arr[chicken][0] = i;
-                    chic_arr[chicken][1] = j;
-                }
-
-                // 집 위치 모으기
                 if (A[i][j] == 1) {
                     home++;
                     home_arr[home][0] = i;
                     home_arr[home][1] = j;
                 }
+                if (A[i][j] == 2) {
+                    chicken++;
+                    chick_arr[chicken][0] = i;
+                    chick_arr[chicken][1] = j;
+                }
             }
         }
-        erase_cnt = chicken - M;
-        // 치킨집 제거
-        dfs(1, 0);
+
+        // M 개 치킨집 고르기
+        rec_func(1);
         System.out.println(ans);
     }
 
