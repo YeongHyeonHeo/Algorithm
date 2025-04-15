5e2c3c621ababd97be8acdbd0412067356fb4803
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-04-15 17:06:30 +0900

    [Gold V] Title: 최소비용 구하기, Time: 512 ms, Memory: 53232 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -6,20 +6,18 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
 
     static class Edge {
-        public int to, weight;
-
-        public Edge(int _to, int _weight) {
-            this.to = _to;
-            this.weight = _weight;
+        int to, weight;
+        Edge(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
         }
     }
 
     static class Info {
-        public int idx, dist;
-
-        public Info(int _idx, int _dist) {
-            this.idx = _idx;
-            this.dist = _dist;
+        int idx, dist;
+        Info(int idx, int dist) {
+            this.idx = idx;
+            this.dist = dist;
         }
     }
 
@@ -32,11 +30,9 @@ public class Main {
         M = scan.nextInt();
         dist = new int[N + 1];
         edges = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) edges[i] = new ArrayList<Edge>();
-        for (int i = 1; i <= M; i++) {
-            int from = scan.nextInt();
-            int to = scan.nextInt();
-            int weight = scan.nextInt();
+        for (int i = 1; i <= N; i++) edges[i] = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            int from = scan.nextInt(), to = scan.nextInt(), weight = scan.nextInt();
             edges[from].add(new Edge(to, weight));
         }
         start = scan.nextInt();
@@ -44,23 +40,16 @@ public class Main {
     }
 
     static void dijkstra(int start) {
-        // 모든 정점까지에 대한 거리를 무한대로 초기화
         for (int i = 1; i <= N; i++) dist[i] = Integer.MAX_VALUE;
-
-        // 최소 힙 생성
         PriorityQueue<Info> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.dist));
-
-        // 시작점에 대한 정보(Information)을 기록에 추가하고, 거리 배열(dist)에 갱신
         pq.add(new Info(start, 0));
         dist[start] = 0;
 
-        // 거리 정보들이 모두 소진될 때까지 거리 갱신을 반복
         while (!pq.isEmpty()) {
             Info info = pq.poll();
             if (dist[info.idx] < info.dist) continue;
             for (Edge e : edges[info.idx]) {
                 if (dist[info.idx] + e.weight >= dist[e.to]) continue;
-
                 dist[e.to] = dist[info.idx] + e.weight;
                 pq.add(new Info(e.to, dist[e.to]));
             }
