5ab2978afc9a13ac27887e687c3fdb9dea794f7d
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-04-20 21:20:42 +0900

    [Silver I] Title: 카드 구매하기, Time: 136 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static int[] P, Dy;
+
+    static void input() {
+        N = scan.nextInt();
+        P = new int[N + 1];
+        for (int i = 1; i <= N; i++) P[i] = scan.nextInt();
+    }
+
+    static void pro() {
+        Dy = new int[N + 1];
+        Dy[0] = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < i; j++) {
+                Dy[i] = Math.max(Dy[i], Dy[j] + P[i-j]);
+            }
+        }
+        System.out.println(Dy[N]);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
