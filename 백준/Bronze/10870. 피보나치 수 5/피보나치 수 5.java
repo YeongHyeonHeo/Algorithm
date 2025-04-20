73f263dd6ca8fd52a13609b9e36a0f13b70e31cf
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-04-20 15:13:43 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 104 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static int[] Dy;
+
+    static void input() {
+        N = scan.nextInt();
+    }
+
+    static void pro() {
+        Dy = new int[N + 2];
+        Dy[0] = 0;
+        Dy[1] = 1;
+        for (int i = 2; i <= N; i++) Dy[i] = Dy[i-1] + Dy[i-2];
+        System.out.println(Dy[N]);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
