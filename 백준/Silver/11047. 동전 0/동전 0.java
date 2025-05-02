cc2ee1526a7671f9e4e8d468ef97b2081436c39a
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-02 23:46:39 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, K;
+    static int[] A;
+
+    static void input() {
+        N = scan.nextInt();
+        K = scan.nextInt();
+        A = new int[N];
+        for (int i = 0; i < N; i++) A[i] = scan.nextInt();
+    }
+
+    static void pro() {
+        int cnt = 0;
+        for (int i = N-1; i >= 0; i--) {
+            cnt += K / A[i];
+            K %= A[i];
+        }
+        System.out.println(cnt);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
