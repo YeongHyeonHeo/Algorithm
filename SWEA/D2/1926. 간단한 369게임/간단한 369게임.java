36859817dc9f9b8467b7b2b9a2f5bb202fb7b07a
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-04 18:49:55 +0900

    [D2] Title: 간단한 369게임, Time: 70 ms, Memory: 24,064 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+
+    static void input() {
+        N = scan.nextInt();
+    }
+
+    static void pro() {
+        for (int i = 1; i <= N; i++) {
+            String str = Integer.toString(i);
+            int cnt = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == '3' || str.charAt(j) == '6' || str.charAt(j) == '9') cnt++;
+            }
+            if (cnt == 0) sb.append(str).append(' ');
+            else {
+                for (int k = 0; k < cnt; k++) sb.append('-');
+                sb.append(' ');
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
