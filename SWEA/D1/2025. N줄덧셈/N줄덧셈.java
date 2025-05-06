5aa51ded91a8a7b7d9d6c7b6802f2f26fc2d1b6d
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-06 18:55:28 +0900

    [D1] Title: N줄덧셈, Time: 76 ms, Memory: 24,192 KB -BaekjoonHub

---
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+
+    static void input() {
+        N = scan.nextInt();
+    }
+
+    static void pro() {
+        int sum = 0;
+        for (int i = 1; i <= N; i++) sum += i;
+        sb.append(sum);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+        System.out.println(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
---
+++ SWEA/D1/2025. N줄덧셈/README.md
@@ -0,0 +1,15 @@
+# [D1] N줄덧셈 - 2025 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QFZtaAscDFAUq) 
+
+### 성능 요약
+
+메모리: 24,192 KB, 시간: 76 ms, 코드길이: 1,495 Bytes
+
+### 제출 일자
+
+2025-05-06 18:55
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
