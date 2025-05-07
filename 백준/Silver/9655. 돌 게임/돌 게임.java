cf6583365a14854df949d5febf0a186daf6b2f46
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-07 20:47:43 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+
+    static int N;
+
+    static void input() {
+        N = scan.nextInt();
+    }
+
+    static void pro() {
+        if (N % 2 == 0) System.out.println("CY");
+        else System.out.println("SK");
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
