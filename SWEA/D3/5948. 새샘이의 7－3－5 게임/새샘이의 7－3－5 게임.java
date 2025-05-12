c85e3567e73c6590657ff7dba902b52b40fb5205
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-12 21:51:31 +0900

    [D3] Title: 새샘이의 7-3-5 게임, Time: 274 ms, Memory: 52,256 KB -BaekjoonHub

--- SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.java
+++ SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.java
@@ -5,23 +5,28 @@ public class Solution {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int[] selected, A, sumArr;
-    static int idx;
+    static int[] selected, A;
+    static ArrayList<Integer> sumArr;
 
     static void input() {
         selected = new int[4];
         A = new int[8];
-        sumArr = new int[35];
+        sumArr = new ArrayList<>();
         for (int i = 1; i <= 7; i++) A[i] = scan.nextInt();
-        idx = 0;
     }
 
     static void rec_func(int k) {
         if (k == 4) {
             int sum = 0;
+            boolean isValid = true;
             for (int i = 1; i <= 3; i++) sum += A[selected[i]];
-            sumArr[idx] = sum;
-            idx++;
+            for (int num : sumArr) {
+                if (num == sum) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) sumArr.add(sum);
         } else {
             for (int cand = selected[k-1]+1; cand <= 7; cand++) {
                 selected[k] = cand;
@@ -32,17 +37,8 @@ public class Solution {
 
     static void pro() {
         rec_func(1);
-        Arrays.sort(sumArr);
-        int n = 1;
-        int min = sumArr[34];
-        for (int i = 33; i >= 0; i--) {
-            if (min > sumArr[i]) {
-                min = sumArr[i];
-                n++;
-            }
-            if (n == 5) break;
-        }
-        sb.append(min).append('\n');
+        sumArr.sort(Collections.reverseOrder());
+        sb.append(sumArr.get(4)).append('\n');
     }
 
     public static void main(String[] args) {
