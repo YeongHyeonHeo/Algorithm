80702f86077aabbac35e2849a772dc10b6b2418f
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-12 22:23:22 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 126 ms, Memory: 36,468 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static ArrayList<Integer> A;
+
+    static void input() {
+        N = scan.nextInt();
+        A = new ArrayList<>();
+        for (int i = 0; i < N; i++) A.add(scan.nextInt());
+    }
+
+    static void pro() {
+        int M = scan.nextInt();
+        while (M-- > 0) {
+            String command = scan.next();
+            if (command.equals("I")) {
+                int x = scan.nextInt();
+                int y = scan.nextInt();
+                for (int i = 0; i < y; i++) {
+                    int code = scan.nextInt();
+                    A.add(x, code);
+                    x++;
+                }
+            } else if (command.equals("D")) {
+                int x = scan.nextInt();
+                int y = scan.nextInt();
+                for (int i = 0; i < y; i++) A.remove(x);
+            } else {
+                int y = scan.nextInt();
+                for (int i = 0; i < y; i++) {
+                    int s = scan.nextInt();
+                    A.add(s);
+                }
+            }
+        }
+        for (int i = 0; i < 10; i++) sb.append(A.get(i)).append(' ');
+        sb.append('\n');
+    }
+
+    public static void main(String[] args) {
+        for (int t = 1; t <= 10; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
