ccf0d786958c9e57bfb02f5efc462459c8a1ce57
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-14 18:00:24 +0900

    [D3] Title: 상원이의 연속 합, Time: 354 ms, Memory: 25,728 KB -BaekjoonHub

---
+++ SWEA/D3/7510. 상원이의 연속 합/상원이의 연속 합.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+
+    static void input() {
+        N = scan.nextInt();
+    }
+
+    static void pro() {
+        int R = 0, sum = 0, ans = 0;
+        for (int L = 1; L <= N; L++) {
+            sum -= L - 1;
+            while (R + 1 <= N && sum < N) {
+                R++;
+                sum += R;
+            }
+            if (sum == N) ans++;
+        }
+        sb.append(ans).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
