6b0e735a2bdc08c11eae2aa9131070a5b73382f0
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-15 21:40:09 +0900

    [D3] Title: 최장 경로, Time: 94 ms, Memory: 27,520 KB -BaekjoonHub

---
+++ SWEA/D3/2814. 최장 경로/최장 경로.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M, ans;
+    static ArrayList<Integer>[] adj;
+    static boolean[] visited;
+
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        adj = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) adj[i] = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            adj[x].add(y);
+            adj[y].add(x);
+        }
+        visited = new boolean[N + 1];
+        ans = 0;
+    }
+
+    static void dfs(int x, int cnt) {
+        visited[x] = true;
+        for (int y : adj[x]) {
+            if (visited[y]) continue;
+            dfs(y, cnt+1);
+            visited[y] = false;
+        }
+        ans = Math.max(ans, cnt);
+    }
+
+    static void pro() {
+        for (int i = 1; i <= N; i++) {
+            dfs(i, 1);
+            visited[i] = false;
+        }
+        sb.append(ans).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
