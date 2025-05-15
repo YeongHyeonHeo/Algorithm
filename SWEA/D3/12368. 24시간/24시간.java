8dbae1ba7afb660d6f2021a04ac8f20f67941740
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-15 18:27:51 +0900

    [D3] Title: 24시간, Time: 75 ms, Memory: 25,472 KB -BaekjoonHub

---
+++ SWEA/D3/12368. 24시간/24시간.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int A, B;
+
+    static void input() {
+        A = scan.nextInt();
+        B = scan.nextInt();
+    }
+
+    static void pro() {
+        if (A + B < 24) sb.append(A + B ).append('\n');
+        else sb.append(A + B - 24).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
