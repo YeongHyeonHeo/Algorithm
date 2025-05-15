c13fff45abde59907ec1d548e23d352ec02f447b
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-15 18:03:22 +0900

    [D3] Title: 조별과제, Time: 96 ms, Memory: 26,880 KB -BaekjoonHub

---
+++ SWEA/D3/13218. 조별과제/README.md
@@ -0,0 +1,15 @@
+# [D3] 조별과제 - 13218 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXzjvCCq-PwDFASs) 
+
+### 성능 요약
+
+메모리: 26,880 KB, 시간: 96 ms, 코드길이: 1,605 Bytes
+
+### 제출 일자
+
+2025-05-15 18:03
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/13218. 조별과제/조별과제.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+
+    static void input() {
+        N = scan.nextInt();
+    }
+
+    static void pro() {
+        int sum = N, cnt = 0;
+        while (sum >= 3) {
+            sum -= 3;
+            cnt++;
+        }
+        sb.append(cnt).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
