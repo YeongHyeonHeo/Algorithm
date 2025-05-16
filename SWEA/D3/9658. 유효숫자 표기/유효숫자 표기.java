81d4aee538f5da31fc64b0c31e365f959904160c
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-16 20:00:57 +0900

    [D3] Title: 유효숫자 표기, Time: 127 ms, Memory: 45,760 KB -BaekjoonHub

---
+++ SWEA/D3/9658. 유효숫자 표기/유효숫자 표기.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static String str;
+
+    static void input() {
+        str = scan.nextLine();
+    }
+
+    static void pro() {
+        String num = "";
+        for (int i = 0; i < 3; i++) num += str.charAt(i);
+        float a = Integer.parseInt(num);
+        float b = (float) Math.round(a/10)/10;
+        if (str.charAt(0) == '9' && str.charAt(1) == '9' && str.charAt(2) >= '5')
+            sb.append(b/10).append('*').append("10^").append(str.length()).append('\n');
+        else sb.append(b).append('*').append("10^").append(str.length()-1).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
