408cbc5b073e61291dda442cff3dffc0d5b74968
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-18 17:42:14 +0900

    [D3] Title: 다양성 측정, Time: 75 ms, Memory: 25,472 KB -BaekjoonHub

---
+++ SWEA/D3/7728. 다양성 측정/다양성 측정.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static String str;
+    static int[] num;
+
+    static void input() {
+        str = scan.nextLine();
+        num = new int[10];
+    }
+
+    static void pro() {
+        for (int i = 0; i < str.length(); i++) num[str.charAt(i)-'0']++;
+        int cnt = 0;
+        for (int i : num) {
+            if (i != 0) cnt++;
+        }
+        sb.append(cnt).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
