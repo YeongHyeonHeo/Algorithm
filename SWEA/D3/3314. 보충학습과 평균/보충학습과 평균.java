c362e334b5245411dbe25d9908ea12383c58542c
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-18 19:51:18 +0900

    [D3] Title: 보충학습과 평균, Time: 129 ms, Memory: 36,048 KB -BaekjoonHub

---
+++ SWEA/D3/3314. 보충학습과 평균/보충학습과 평균.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static void pro() {
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int score = scan.nextInt();
+            if (score < 40) sum += 40;
+            else sum += score;
+        }
+        sb.append(sum/5).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
