635d228d329ca2a7b6dd6b5b0b511859c321d3af
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-18 17:32:31 +0900

    [D3] Title: 일요일, Time: 73 ms, Memory: 24,960 KB -BaekjoonHub

---
+++ SWEA/D3/13229. 일요일/일요일.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static HashMap<String, Integer> map = new HashMap<>();
+
+    static void init() {
+        String[] A = {"SAT", "FRI", "THU", "WED", "TUE", "MON", "SUN"};
+        for (int i = 0; i < A.length; i++) map.put(A[i], i+1);
+    }
+
+    static String S;
+
+    static void input() {
+        S = scan.nextLine();
+    }
+
+    static void pro() {
+        sb.append(map.get(S)).append('\n');
+    }
+
+    public static void main(String[] args) {
+        init();
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
