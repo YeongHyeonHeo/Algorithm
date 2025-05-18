148b92ab7894fb5a582d1531fbc60594880135f5
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-18 20:48:38 +0900

    [D3] Title: Calkin-Wilf tree 1, Time: 129 ms, Memory: 34,600 KB -BaekjoonHub

---
+++ SWEA/D3/11688. Calkin－Wilf tree 1/Calkin－Wilf tree 1.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static String str;
+
+    static void input() {
+        str = scan.nextLine();
+    }
+
+    static void pro() {
+        int a = 1, b = 1;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'L') b += a;
+            else a += b;
+        }
+        sb.append(a).append(' ').append(b).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
