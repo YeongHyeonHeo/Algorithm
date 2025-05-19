7a4231fa1358baedfdf4fd2952fb28e6e8bc51f1
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-19 19:01:28 +0900

    [D3] Title: 두가지 빵의 딜레마, Time: 92 ms, Memory: 26,368 KB -BaekjoonHub

---
+++ SWEA/D3/5162. 두가지 빵의 딜레마/두가지 빵의 딜레마.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int A, B, C;
+
+    static void input() {
+        A = scan.nextInt();
+        B = scan.nextInt();
+        C = scan.nextInt();
+    }
+
+    static void pro() {
+        int min = Math.min(A, B);
+        sb.append(C/min).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
