dc7bb2b72797299c74b71ebfc837f71027707d57
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-19 18:55:48 +0900

    [D3] Title: 공과 잡초, Time: 72 ms, Memory: 25,600 KB -BaekjoonHub

---
+++ SWEA/D3/14555. 공과 잡초/공과 잡초.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static String str;
+
+    static void input() {
+        str = scan.nextLine();
+    }
+
+    static void pro() {
+        int cnt = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(' || str.charAt(i) == ')') {
+                cnt++;
+                if (i >= 1 && str.charAt(i-1) == '(') cnt--;
+            }
+        }
+        sb.append(cnt).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
