53cae167737f1a30131ee60081ca31178491c69a
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-19 19:12:51 +0900

    [D3] Title: 반반, Time: 76 ms, Memory: 25,088 KB -BaekjoonHub

---
+++ SWEA/D3/11856. 반반/README.md
@@ -0,0 +1,15 @@
+# [D3] 반반 - 11856 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXjS1GXqZ8gDFATi) 
+
+### 성능 요약
+
+메모리: 25,088 KB, 시간: 76 ms, 코드길이: 1,842 Bytes
+
+### 제출 일자
+
+2025-05-19 19:12
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/11856. 반반/반반.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static String str;
+    static int[] alp;
+
+    static void input() {
+        str = scan.nextLine();
+        alp = new int[26];
+    }
+
+    static void pro() {
+        for (int i = 0; i < 4; i++) alp[str.charAt(i) - 'A']++;
+        boolean flag = true;
+        for (int i = 0; i < 26; i++) {
+            if (alp[i] != 2 && alp[i] != 0) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) sb.append("Yes\n");
+        else sb.append("No\n");
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
