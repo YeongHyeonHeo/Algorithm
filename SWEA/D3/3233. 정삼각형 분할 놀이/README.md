3bd368ffd167c1b08a97d10e2fe943eb76d41634
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-21 21:07:59 +0900

    [D3] Title: 정삼각형 분할 놀이, Time: 103 ms, Memory: 27,904 KB -BaekjoonHub

---
+++ SWEA/D3/3233. 정삼각형 분할 놀이/README.md
@@ -0,0 +1,15 @@
+# [D3] 정삼각형 분할 놀이 - 3233 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWAe5G8afT0DFAUw) 
+
+### 성능 요약
+
+메모리: 27,904 KB, 시간: 103 ms, 코드길이: 1,621 Bytes
+
+### 제출 일자
+
+2025-05-21 21:07
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/3233. 정삼각형 분할 놀이/정삼각형 분할 놀이.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static long A, B;
+
+    static void input() {
+        A = scan.nextLong();
+        B = scan.nextLong();
+    }
+
+    static void pro() {
+        sb.append((A*A) / (B*B)).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
