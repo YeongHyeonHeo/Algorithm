123cf0f2fc4e2b5de4ef8cbf0f4392d2a86def10
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-21 20:08:18 +0900

    [D3] Title: 기차 사이의 파리, Time: 147 ms, Memory: 37,712 KB -BaekjoonHub

---
+++ SWEA/D3/6019. 기차 사이의 파리/README.md
@@ -0,0 +1,15 @@
+# [D3] 기차 사이의 파리 - 6019 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWajaTmaZw4DFAWM) 
+
+### 성능 요약
+
+메모리: 37,712 KB, 시간: 147 ms, 코드길이: 1,713 Bytes
+
+### 제출 일자
+
+2025-05-21 20:08
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/6019. 기차 사이의 파리/기차 사이의 파리.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int D, A, B, F;
+
+    static void input() {
+        D = scan.nextInt();
+        A = scan.nextInt();
+        B = scan.nextInt();
+        F = scan.nextInt();
+    }
+
+    static void pro() {
+        double s = (double) D / (A + B);
+        sb.append(s * F).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
