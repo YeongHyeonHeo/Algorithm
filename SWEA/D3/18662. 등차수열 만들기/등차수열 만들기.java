c63a50539bf13bb97f188b54990c0291454bc87e
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-23 00:58:24 +0900

    [D3] Title: 등차수열 만들기, Time: 85 ms, Memory: 25,984 KB -BaekjoonHub

---
+++ SWEA/D3/18662. 등차수열 만들기/등차수열 만들기.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int a, b, c;
+
+    static void input() {
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+    }
+
+    static void pro() {
+        sb.append((float)Math.abs((b-a)-(c-b))/2).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
