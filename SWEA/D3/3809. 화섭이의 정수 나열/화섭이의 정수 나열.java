c5424dd041207b96acfcb6e6dce6177b7a650c41
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-05-23 17:28:02 +0900

    [D3] Title: 화섭이의 정수 나열, Time: 211 ms, Memory: 95,044 KB -BaekjoonHub

---
+++ SWEA/D3/3809. 화섭이의 정수 나열/화섭이의 정수 나열.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static String str;
+
+    static void input() {
+        N = scan.nextInt();
+        str = "";
+        for (int i = 0; i < N; i++) str += Integer.toString(scan.nextInt());
+    }
+
+    static void pro() {
+        int i = 0;
+        while (true) {
+            if (!str.contains(Integer.toString(i))) break;
+            i++;
+        }
+        sb.append(i).append('\n');
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        for (int t = 1; t <= T; t++) {
+            sb.append('#').append(t).append(' ');
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
