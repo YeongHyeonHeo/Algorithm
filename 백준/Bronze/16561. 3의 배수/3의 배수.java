7eb3bd70f8f608710672b76d958bf606d72b52d4
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-06-08 20:34:29 +0900

    [Bronze II] Title: 3의 배수, Time: 452 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/16561. 3의 배수/3의 배수.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int n, cnt;
+
+    static void input() {
+        n = scan.nextInt();
+    }
+
+    static void pro() {
+        for (int i = 3; i <= n - 6; i += 3) {
+            for (int j = 3; j <= n - 3 - i; j += 3) {
+                for (int k = 3; k <= n - i - j; k += 3) {
+                    if (i + j + k == n) cnt++;
+                }
+            }
+        }
+        sb.append(cnt);
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
