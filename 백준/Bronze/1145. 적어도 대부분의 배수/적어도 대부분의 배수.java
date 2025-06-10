2ff4fc3941f884594cec31c3c7ec076abf97cff4
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-06-10 16:16:58 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 128 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int[] num;
+
+    static void input() {
+        num = new int[5];
+        for (int i = 0; i < 5; i++) num[i] = scan.nextInt();
+    }
+
+    static void pro() {
+        Arrays.sort(num);
+        int ans = num[0];
+        while (true) {
+            int cnt = 0;
+            for (int n : num) {
+                if (ans % n == 0) cnt++;
+            }
+            if (cnt >= 3) break;
+            ans++;
+        }
+        sb.append(ans);
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
