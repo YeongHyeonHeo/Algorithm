1c1970c83fd2e122580bc6ee2a77d3915b22d29e
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-06-12 22:35:00 +0900

    [Silver V] Title: 문서 검색, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,16 +1,71 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String doc = sc.nextLine();
-        String search = sc.nextLine();
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
 
-        String str = doc.replace(search, "*");
-        int ans = 0;
-        for (int i = 0; i < str.length(); i++){
-            if (str.charAt(i) == '*') ans++;
+    static String word, find;
+
+    static void input() {
+        word = scan.nextLine();
+        find = scan.nextLine();
+    }
+
+    static void pro() {
+        int cnt = 0;
+        for (int i = 0; i < word.length() - find.length() + 1; i++) {
+            boolean flag = true;
+            for (int j = 0; j < find.length(); j++) {
+                if (word.charAt(i + j) != find.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                cnt++;
+                i += find.length() - 1;
+            }
         }
-        System.out.println(ans);
+        System.out.println(cnt);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
 }
