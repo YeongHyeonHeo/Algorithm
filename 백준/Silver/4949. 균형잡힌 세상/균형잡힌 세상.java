e7341a40300d30fe2f49cadb2a380bfae83f6b7f
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-07-11 16:08:41 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 176 ms, Memory: 19052 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static String str;
+    static Stack<Character> stack;
+
+    static void input() {
+        stack = new Stack<>();
+    }
+
+    static void pro() {
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '(' || c == '[') stack.push(c);
+            else if (c == ')') {
+                if (stack.empty() || stack.peek() != '(') {
+                    sb.append("no\n");
+                    return;
+                }
+                else stack.pop();
+            }
+            else if (c == ']') {
+                if (stack.empty() || stack.peek() != '[') {
+                    sb.append("no\n");
+                    return;
+                }
+                else stack.pop();
+            }
+        }
+
+        if (stack.empty()) sb.append("yes\n");
+        else sb.append("no\n");
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            str = scan.nextLine();
+            if (str.length() == 1) break;
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
