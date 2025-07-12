7af8b059e6293240fbcaf0a25725841981f5efb3
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-07-12 21:27:30 +0900

    [난이도 1] Title: 인공지능 청소기, Time: 0.07 ms, Memory: 31.47 MB -BaekjoonHub

--- goormlevel/43068/1. 인공지능 청소기/인공지능 청소기.java
+++ goormlevel/43068/1. 인공지능 청소기/인공지능 청소기.java
@@ -16,8 +16,7 @@ class Main {
 	static void pro() {
 		int sum = Math.abs(X) + Math.abs(Y);
 		if (sum > N) sb.append("NO\n");
-		else if (sum % 2 == 0 && N % 2 == 0) sb.append("YES\n");
-		else if (sum % 2 != 0 && N % 2 != 0) sb.append("YES\n");
+		else if (sum % 2 == N % 2) sb.append("YES\n");
 		else sb.append("NO\n");
 	}
 	
@@ -25,32 +24,3 @@ class Main {
 		int T = scan.nextInt();
 		while (T-- > 0) {
 			input();
-			pro();
-		}
-		System.out.print(sb);
-	}
-
-	static class FastReader {
-		BufferedReader br;
-		StringTokenizer st;
-
-		public FastReader() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
-
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
-
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
-	}
-}
