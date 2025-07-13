53607ce59687dc05030f75143b535d80e5ee0b7e
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-07-13 16:46:39 +0900

    [Silver IV] Title: 괄호, Time: 120 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static String str;
+
+    static void input() {
+        str = scan.nextLine();
+    }
+
+    static void pro() {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') stack.push('(');
+            else {
+                if (stack.isEmpty()) {
+                    sb.append("NO\n");
+                    return;
+                }
+                else stack.pop();
+            }
+        }
+
+        if (stack.isEmpty()) sb.append("YES\n");
+        else sb.append("NO\n");
+    }
+
+    public static void main(String[] args) {
+        int T = scan.nextInt();
+        while (T-- > 0) {
+            input();
+            pro();
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
