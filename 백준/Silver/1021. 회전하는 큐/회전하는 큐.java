0b346af3b1639291f4563a8b2f4b5780fc9180c8
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-09-05 14:22:45 +0900

    [Silver III] Title: 회전하는 큐, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[] arr;
+    static Deque<Integer> deque;
+
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        arr = new int[M];
+        for (int i = 0; i < M; i++) arr[i] = scan.nextInt();
+        deque = new LinkedList<>();
+    }
+
+    static int findIdx(int find) {
+        int idx = 0;
+        for (int num : deque) {
+            if (num == find) break;
+            idx++;
+        }
+        return idx;
+    }
+
+    static void pro() {
+        for (int i = 1; i <= N; i++) deque.add(i);
+
+        int ans = 0;
+        for (int find : arr) {
+            int idx = findIdx(find);
+            if (idx > deque.size()/2) {
+                // 오른쪽 이동
+                while (deque.peek() != find) {
+                    deque.addFirst(deque.pollLast());
+                    ans++;
+                }
+            } else {
+                // 왼쪽 이동
+                while (deque.peek() != find) {
+                    deque.add(deque.poll());
+                    ans++;
+                }
+            }
+            deque.poll();
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
