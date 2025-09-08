17ffaac1cf4bcc7fa377e54269b651aa69c34d83
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-09-08 10:52:32 +0900

    [Silver IV] Title: 덱 2, Time: 1092 ms, Memory: 257012 KB -BaekjoonHub

---
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+
+    static void input() {
+        N = scan.nextInt();
+    }
+
+    static void pro() {
+        Deque<Integer> deque = new LinkedList<>();
+
+        while (N-- > 0) {
+            int cmd = scan.nextInt();
+            switch (cmd) {
+                case 1 :
+                    deque.addFirst(scan.nextInt());
+                    break;
+                case 2 :
+                    deque.addLast(scan.nextInt());
+                    break;
+                case 3 :
+                    if (deque.isEmpty()) sb.append("-1\n");
+                    else sb.append(deque.removeFirst()).append('\n');
+                    break;
+                case 4 :
+                    if (deque.isEmpty()) sb.append("-1\n");
+                    else sb.append(deque.removeLast()).append('\n');
+                    break;
+                case 5 :
+                    sb.append(deque.size()).append('\n');
+                    break;
+                case 6 :
+                    if (deque.isEmpty()) sb.append("1\n");
+                    else sb.append("0\n");
+                    break;
+                case 7 :
+                    if (deque.isEmpty()) sb.append("-1\n");
+                    else sb.append(deque.peekFirst()).append('\n');
+                    break;
+                case 8 :
+                    if (deque.isEmpty()) sb.append("-1\n");
+                    else sb.append(deque.peekLast()).append('\n');
+            }
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
