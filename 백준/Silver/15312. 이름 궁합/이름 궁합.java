10b2ab453fdca39771702f5589eec4db215a01db
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-09-11 09:52:30 +0900

    [Silver V] Title: 이름 궁합, Time: 124 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Silver/15312. 이름 궁합/이름 궁합.java
+++ 백준/Silver/15312. 이름 궁합/이름 궁합.java
@@ -6,29 +6,23 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
 
     static String A, B;
-    static int[] arr, arrClone;
+    static int[] arr;
     static int[] alp = {3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1};
 
     static void input() {
         A = scan.nextLine();
         B = scan.nextLine();
         arr = new int[A.length() * 2];
-        arrClone = new int[A.length() * 2];
         int idx = 0;
         for (int i = 0; i < A.length(); i++) {
-            arrClone[idx++] = alp[A.charAt(i)-'A'];
-            arrClone[idx++] = alp[B.charAt(i)-'A'];
+            arr[idx++] = alp[A.charAt(i)-'A'];
+            arr[idx++] = alp[B.charAt(i)-'A'];
         }
     }
 
     static void pro() {
         for (int cnt = A.length()*2-1; cnt >=2 ; cnt--) {
-            for (int i = 0; i < cnt; i++) {
-                int num = arrClone[i] + arrClone[i+1];
-                if (num >= 10) num %= 10;
-                arr[i] = num;
-            }
-            for (int i = 0; i < cnt; i++) arrClone[i] = arr[i];
+            for (int i = 0; i < cnt; i++) arr[i] = (arr[i] + arr[i+1]) % 10;
         }
         sb.append(arr[0]).append(arr[1]);
         System.out.print(sb);
