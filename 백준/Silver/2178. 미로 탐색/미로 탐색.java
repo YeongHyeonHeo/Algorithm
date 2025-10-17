533dd07a617b78db023dbd453e2f198f1cf03dac
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-10-17 17:12:24 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14712 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -6,38 +6,42 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
 
     static int N, M;
-    static String[] A;
+    static String[] a;
+    static int[][] dist;
     static boolean[][] visit;
     static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-    static int[][] dist;
 
     static void input() {
         N = scan.nextInt();
         M = scan.nextInt();
-        A = new String[N];
-        for (int i = 0; i < N; i++) A[i] = scan.nextLine();
-        visit = new boolean[N][M];
+        a =  new String[N];
+        for (int i = 0; i < N; i++) a[i] = scan.nextLine();
         dist = new int[N][M];
+        visit = new boolean[N][M];
     }
 
     static void bfs(int x, int y) {
-        Queue<Integer> que = new LinkedList<>();
-        que.add(x);
-        que.add(y);
-        visit[x][y] = true;
-        dist[x][y] = 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) dist[i][j] = -1;
+        }
 
-        while (!que.isEmpty()) {
-            x = que.poll();
-            y = que.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dir[i][0];
-                int ny = y + dir[i][1];
+        Queue<Integer> Q = new LinkedList<>();
+        Q.add(x);
+        Q.add(y);
+        dist[x][y] = 1;
+        visit[x][y] = true;
+
+        while (!Q.isEmpty()) {
+            x = Q.poll();
+            y = Q.poll();
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dir[k][0];
+                int ny=  y + dir[k][1];
                 if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if (a[nx].charAt(ny) == '0') continue;
                 if (visit[nx][ny]) continue;
-                if (A[nx].charAt(ny) == '0') continue;
-                que.add(nx);
-                que.add(ny);
+                Q.add(nx);
+                Q.add(ny);
                 visit[nx][ny] = true;
                 dist[nx][ny] = dist[x][y] + 1;
             }
@@ -47,12 +51,13 @@ public class Main {
     static void pro() {
         bfs(0, 0);
         sb.append(dist[N-1][M-1]);
+        System.out.print(sb);
     }
 
     public static void main(String[] args) {
         input();
         pro();
-        System.out.print(sb);
+
     }
 
     static class FastReader {
