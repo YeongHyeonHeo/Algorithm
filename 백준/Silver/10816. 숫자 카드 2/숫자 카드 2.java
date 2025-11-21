071844cd1ab4c0a632f4879f716b3ee1d7b7eaa0
Heo YeongHyeon <123229998+YeongHyeonHeo@users.noreply.github.com>
2025-11-21 15:24:17 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1012 ms, Memory: 174012 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -6,47 +6,25 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
 
     static int N, M;
-    static int[] A, B;
+    // HashMap 이용
+    // key: 카드에 적힌 숫자, value: 해당 카드 개수
+    static HashMap<Integer, Integer> hashMap = new HashMap<>();
 
     static void input() {
         N = scan.nextInt();
-        A = new int[N + 1];
-        for (int i = 1; i <= N; i++) A[i] = scan.nextInt();
+        while (N-- > 0) {
+            int num = scan.nextInt();
+            // 중복된 카드라면, value(카드 개수) 1 증가
+            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+        }
         M = scan.nextInt();
-        B = new int[M + 1];
-        for (int i = 1; i <= M; i++) B[i] = scan.nextInt();
-    }
-
-    static int lower_bound(int[] A, int L, int R, int X) {
-        int res = L - 1;
-        while (L <= R) {
-            int mid = (L + R) / 2;
-            if (A[mid] < X) {
-                res = mid;
-                L = mid + 1;
-            } else R = mid - 1;
-        }
-        return res;
-    }
-
-    static int upper_bound(int[] A, int L, int R, int X) {
-        int res = R + 1;
-        while (L <= R) {
-            int mid = (L + R) / 2;
-            if (A[mid] > X) {
-                res = mid;
-                R = mid - 1;
-            } else L = mid + 1;
-        }
-        return res;
     }
 
     static void pro() {
-        Arrays.sort(A, 1, N + 1);
-        for (int i = 1; i <= M; i++) {
-            int lower = lower_bound(A, 1, N, B[i]);
-            int upper = upper_bound(A, 1, N, B[i]);
-            sb.append(upper-lower-1).append(' ');
+        // 각 숫자카드 몇 개인지 출력
+        while (M-- > 0) {
+            int num = scan.nextInt();
+            sb.append(hashMap.getOrDefault(num, 0)).append(' ');
         }
         System.out.println(sb);
     }
@@ -77,24 +55,6 @@ public class Main {
 
         int nextInt() {
             return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
         }
     }
 }
